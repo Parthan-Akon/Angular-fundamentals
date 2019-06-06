398a3e1c586e26fed73604d3267faacd5fa165be
Parthan <parthan@techcreations.in>
2019-06-06 18:01:08 +0530

    added required

--- angular/src/app/user/login.component.ts
+++ angular/src/app/user/login.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
-
+import { Router } from '@angular/router'
 @Component({
     templateUrl:'./login.component.html'
 })
 
 export class LoginComponent {
 
-    constructor(private authService:AuthService){
+    constructor(private authService:AuthService, private router:Router){
 
     }
 userName
@@ -17,5 +17,10 @@ password
 
 login(formValues){
    this.authService.loginUser(formValues.userName,formValues.password)
+   this.router.navigate(['events'])
 }
+cancel(){
+    this.router.navigate(['events'])
+}
+
 }
